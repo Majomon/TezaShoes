f67e439605bc115d935fa19e106a97c2010f07d3
Mauri <mauri.monzon91@gmail.com>
2023-12-05 13:24:13 -0300

    base

---
+++ api/src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+/* const routerUser = require("./routes/users.routes");
+const routerProperty = require("./routes/property.routes");
+const routerResend = require("./routes/resend.routes");
+const routerSearch = require("./routes/search.routes") */
+
+//Rutas
+
+const app = express();
+//
+//Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+// Routes
+/* app.use("/users", routerUser);
+app.use("/properties", routerProperty);
+app.use("/resend", routerResend);
+app.use("/search", routerSearch)
+ */
+module.exports = app;

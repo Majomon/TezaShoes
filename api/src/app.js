20987d0da69f617387fdb9f3fb6592c18d3c2251
Mauri <mauri.monzon91@gmail.com>
2023-12-06 22:01:13 -0300

    user

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-//
+//asdasdasd
 // Routes
 /* app.use("/users", routerUser);
 app.use("/properties", routerProperty);

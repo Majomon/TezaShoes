8f4e2a89720d9a9dd501ae8d5db507fee9dbd425
Mauri <mauri.monzon91@gmail.com>
2023-12-09 09:30:50 -0300

    Ruta PUT usuario por id - Hecho

--- api/src/handlers/products/handlerPutProductId.js
+++ api/src/handlers/products/handlerPutProductId.js
@@ -4,8 +4,8 @@ const handlerPutProductId = async (req, res) => {
   const {id} = req.params;
   const {name, category, options, price, description, images, settlement,promotion} = req.body;
   try {
-    const putProduct = await controllerPutProductId({id,name, category, options, price, description, images, settlement,promotion});
-    res.status(200).json(putProduct);
+    const updateProduct = await controllerPutProductId({id,name, category, options, price, description, images, settlement,promotion});
+    res.status(200).json(updateProduct);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }

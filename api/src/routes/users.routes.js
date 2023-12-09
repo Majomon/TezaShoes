24a6ec16cef68f07019eb1e045788cc6c5292aa9
Mauri <mauri.monzon91@gmail.com>
2023-12-09 09:17:15 -0300

    Ruta usuario por ID - Hecho

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,9 +1,12 @@
 const handlerGetAllUsers = require("../handlers/users/handlerGetAllUsers");
+const handlerGetUserId = require("../handlers/users/handlerGetUserId");
 const handlerPostUser = require("../handlers/users/handlerPostUser");
 
 const routerUsers = require("express").Router();
 
 routerUsers.post("/", handlerPostUser);
 routerUsers.get("/", handlerGetAllUsers);
+routerUsers.get("/:id", handlerGetUserId);
+
 
 module.exports = routerUsers;

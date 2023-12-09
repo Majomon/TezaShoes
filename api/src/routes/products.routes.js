15ef5ab0d78de27a7ab00fa41c75c27486bbc761
Mauri <mauri.monzon91@gmail.com>
2023-12-08 23:49:23 -0300

    Hecho ruta para pedir productos por ID

--- api/src/routes/products.routes.js
+++ api/src/routes/products.routes.js
@@ -1,12 +1,11 @@
 const routerProducts = require("express").Router();
-const handlerGetAllProducts = require("../handlers/products/handlerGetProducts");
+
 const handlerPostUser = require("../handlers/products/handlerPostUser");
+const handlerGetAllProducts = require("../handlers/products/handlerGetProducts");
+const handlerGetProductId = require("../handlers/products/handlerGetProductID");
 
 routerProducts.post("/", handlerPostUser);
 routerProducts.get("/", handlerGetAllProducts);
-/* routerProducts.get("/:id", handlerGetAllProducts); */
-
-
-
+routerProducts.get("/:id", handlerGetProductId);
 
 module.exports = routerProducts;

1f7b75a1b199bf7f5218c20827795a19926ca21e
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-11 12:05:39 -0300

    colocando carpeta svg

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,5 +1,12 @@
+import CardInfoIcons from "./components/CardInfoIcons";
+import NavMenu from "./components/NavMenu";
+
 export default function Home() {
   return (
-  <h1>PRUEBA</h1>
+    <>
+      <h1>PRUEBA</h1>
+      <CardInfoIcons />
+    </>
+    
   )
 }

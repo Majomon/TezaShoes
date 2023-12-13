8334b48b8870718244b1cc54f499b2646c7cffb9
Camila Manita <camilamanita3@gmail.com>
2023-12-13 13:51:29 -0300

    Hago footer

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Providers } from "./providers";
+import Footer from '@/app/components/Footer/Footer';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Providers>
           {children}
+          <Footer/>
         </Providers>
       </body>
     </html>

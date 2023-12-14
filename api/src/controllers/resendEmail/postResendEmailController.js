85140183a84319872f2bc1a88b4a402f7a4ad986
Mauri <mauri.monzon91@gmail.com>
2023-12-14 16:39:33 -0300

    Hecho ruta para enviar mails desde la page contact

---
+++ api/src/controllers/resendEmail/postResendEmailController.js
@@ -0,0 +1,19 @@
+const { Resend } = require("resend");
+const { EmailTemplate } = require("../../template/templateResendEmail");
+
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const postResendEmailController = async ({ name, email, phone, message }) => {
+  const data = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: ["mauri.monzon91@gmail.com"],
+    subject: "Hello Perro",
+    html: await EmailTemplate({ name, email, phone, message }),
+  });
+
+  return data;
+};
+
+// Exporta la función postResendEmailController
+module.exports = postResendEmailController;

8705cc90b2f9de24e75a15a4634ffaaf57ff6b92
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-16 00:17:47 -0300

    carrusel terminado

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -5,7 +5,6 @@ import CarruselNewProdTwo from '@/components/Carrusel/CarruselNewProdTwo'
 export default function Home() {
   return (
     <>
-      <h1>PRUEBA</h1>
       <CarruselNewProdTwo />
      {/*  <Footer /> */}
     </>

e74928b8c2c20633b54171271d2a77a50c2bf7e6
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-18 22:32:12 -0300

    iconos y links agregados

--- client/src/app/frequentQuestions/page.jsx
+++ client/src/app/frequentQuestions/page.jsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import { IconArrowRight } from "../../../assets/svg/frequentQuestions";
 
 export default function FrequentQuestions(){
     return(
         <div className="w-full min-h-screen px-16 flex mt-10">
-            <Link href="/">Inicio</Link>
-            <p>Preguntas Frecunetes</p>
+            <ul className="flex flex-row gap-x-[5px]">
+                <Link href="/">Inicio</Link>
+                <IconArrowRight />
+                <p>Preguntas Frecuentes</p>
+            </ul>
         </div>
     )
 }

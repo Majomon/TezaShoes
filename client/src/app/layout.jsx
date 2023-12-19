e441161b267101d6ec23e761b234262833836819
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-19 00:16:35 -0300

    añadiendo tipografia

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -4,7 +4,8 @@ import { Providers } from "./providers";
 import Footer from '@/components/Footer/Footer';
 import Nav from "@/components/Navbar/Nav";
 
-const martel = Martel({ weight: ["400", "700"], subsets: ["latin"] });
+const martel = Martel({ weight: ["300","500", "700"], subsets: ["latin"] });
+const cormonrantGaramond = cormonrantGaramond({ weight: ["300","500","700"] })
 
 export const metadata = {
   title: "TezaShoes - Inicio",
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Nav/>
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>

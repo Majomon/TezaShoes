9e1f3315354c5c4b828a45a90865179fc2e76586
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-18 22:22:45 -0300

    empezando la page  de preguntas frecuentes

---
+++ client/src/app/frequentQuestions/page.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FrequentQuestions(){
+    return(
+        <div className="w-full min-h-screen px-16 flex mt-10">
+            <Link href="/">Inicio</Link>
+            <p>Preguntas Frecunetes</p>
+        </div>
+    )
+}

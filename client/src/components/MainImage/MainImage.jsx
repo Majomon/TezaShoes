d84d40f22df10a77054cbd6b821d74fb6a8d0eda
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-19 14:27:01 -0300

    haciendo la mainImage responsive

--- client/src/components/MainImage/MainImage.jsx
+++ client/src/components/MainImage/MainImage.jsx
@@ -1,13 +1,18 @@
 import Image from "next/image"
 import mainImageHome from "../../../assets/image/mainImageHome.png"
+import mainImageHomeMaxSize from "../../../assets/image/mainIMageHomeMaxSize.png"
+import { useEffect } from "react"
 
 export default function MainImage() {
+    useEffect(() => {
+        
+    },[])
+
     return(
 
         <Image
-            src={mainImageHome}
-            height={450}
-            objectFit={"cover"}
+            src={mainImageHomeMaxSize}
+            className=" w-[100%] max-h-[600px]"
         />
 
     )

0339ac34bc420905da996c9be5ba9ffa0b65986c
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-20 19:44:06 -0300

    Modifique el endpoint de PostProduct

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,21 +5,21 @@ import Link from "next/link";
 
 export default function Card({ images, title, price, cantDues, isNew,id }) {
   return (
-    <div className=" w-[300px] h-[380px] rounded-t-lg rounded-b-lg shadow-3xl">
-      <section className="h-[300px] w-[100%] relative">
+    <div className=" w-[300px] h-[410px] rounded-md  shadow-3xl">
+      <section className="w-[100%]  h-[300px] relative">
         <Newlabel isNew={isNew} />
         <Link href={`/detail/${id}`}>
           <Image
-            className="w-[300px] h-[300px] rounded-t-lg rounded-b-none object-cover cursor-pointer"
+            className="w-[300px] h-[300px] rounded-t-md rounded-b-none object-cover cursor-pointer"
             src={images[0]}
             alt="imageCard"
           />
         </Link>
       </section>
-      <section className="h-[80px] flex flex-col justify-between">
-        <div className="flex flex-col justify-center p-[5px] bg-colorWhite-100">
-          <h2 className=" font-semibold uppercase">{title}</h2>
-          <p className=" font-normal ">${price}</p>
+      <section className="h-[110px] flex flex-col justify-between">
+        <div className="w-full h-full flex flex-col justify-between p-2 bg-colorWhite-100">
+          <h2 className="font-semibold uppercase">{title}</h2>
+          <p className="font-normal ">${price}</p>
         </div>
         <p className=" rounded-b-lg bg-colorGold-800 text-center ">
           {cantDues} Cuotas sin interés

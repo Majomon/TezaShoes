ee03a248db3000cce4e4ce4c07ea2998fab054d1
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-20 18:40:21 -0300

    arreglando el newlabel

--- client/src/components/Newlabel/Newlabel.jsx
+++ client/src/components/Newlabel/Newlabel.jsx
@@ -1,11 +1,9 @@
 
-export default function Newlabel(isNew){
-
-    let New = isNew ? "block" : "flex-none";
-   
+export default function Newlabel({isNew}){
+    let newStyle = isNew ? "block" : "hidden";
 
     return(
-        <div className={`bg-colorBlack-400 text-colorWhite-100 w-[80px] h-[45px] flex items-center justify-center absolute z-[100] top-[21px] ${New}`}>
+        <div className={`bg-colorBlack-400 text-colorWhite-100 w-[80px] h-[45px] flex items-center justify-center absolute z-[100] top-[21px] ${newStyle}`}>
             Nuevo
         </div>
     )

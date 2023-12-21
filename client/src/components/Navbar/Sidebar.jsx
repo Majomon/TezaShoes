e9b0a3f0722ec16b125ce3c34f4da4f6ef8a05fc
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-21 20:52:06 -0300

    modificando el style del side bar

--- client/src/components/Navbar/Sidebar.jsx
+++ client/src/components/Navbar/Sidebar.jsx
@@ -24,15 +24,15 @@ const SideBar = ({ isMenuOpen, setIsMenuOpen }) => {
       {/* SIDEBAR */}
       <div
         className={`${
-          isMenuOpen ? "w-60" : "w-0"
+          isMenuOpen ? "w-[300px]" : "w-0"
         } bg-white min-h-screen fixed top-0 left-0 transition-all duration-300 z-30`}
       >
         <div className={`${!isMenuOpen && "hidden"} pt-3`}>
-          <div className="flex flex-row items-center justify-center">
-            <button className="mx-2" onClick={() => setIsMenuOpen(false)}>
+          <div className="flex flex-row items-center justify-start px-5 ">
+            <button className="" onClick={() => setIsMenuOpen(false)}>
               <CgClose size={25} color="black" />
             </button>
-            <Image className="mx-2" src={LogoTeza} alt="Logo Teza Shoes" />
+            <Image className="mx-auto" src={LogoTeza} alt="Logo Teza Shoes" />
           </div>
           <ul>
             {menuItems.map((item) => (

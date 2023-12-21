608bcbc06a440d82d4ae0984b692712c46a33587
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-20 22:49:33 -0300

    haciendo el ordenamiento de mayor y menor

--- client/src/components/Search/Order.jsx
+++ client/src/components/Search/Order.jsx
@@ -1,18 +1,19 @@
-import { IconFilter } from "../../../assets/svg/IconsPageSearch";
 
-export default function Order(){
+export default function Order(setHightPrice){
+
+    const handleOnclickHightPrice = (e) => {
+        const { value } = e.target;
+        setHightPrice(value)
+    }
+
     return(
         <div className="flex sm:flex-row flex-col gap-y-[15px] flex-wrap items-center w-[100%] sm:justify-between">
             <section className="flex flex-row items-center gap-x-[10px]">
                 <p>Ordenar por</p>
-                <select name="select" className="border-2 border-colorBlack-400 w-[150px] h-[45px] outline-none flex items-center justify-center">
+                <select name="select" className="border-2 border-colorBlack-400 w-[150px] h-[45px] outline-none flex items-center justify-center" onClick={handleOnclickHightPrice}>
                     <option value="mayor">Mayor Precio</option>
                     <option value="menor">Menor Precio</option>
                 </select>
-            </section>
-            <section className="flex flex-row gap-x-[5px] cursor-pointer items-center">
-                <IconFilter />
-                <p>Filtro</p>
             </section>
         </div>
     )
--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,23 +1,51 @@
 'use client'
-import Order from "./Order";
 import PageRouting from "../PageRouting/PageRouting";
 import { useStoreProducts } from "@/zustand/store";
-import { useEffect } from "react";
+import { use, useEffect,useState } from "react";
 import Card from "../Card/Card";
+import Order from "./Order";
 
 export default function Search({product}){
+    /* const [valueSelect,setValueSelect] = useState("mayor"); */
     const { setProducts,allProducts } = useStoreProducts();
 
     useEffect(()=>{
         setProducts(product)
     },[product]);
+    
+    const handleOnchangeHightPrice = (e) => {
+        const { value } = e.target;
+        console.log(value)
+        if(value === "mayor"){
+            product.sort((a,b) => b.name - a.name);
+            console.log(product);
+            setProducts(product)
+        }else if(value === "menor"){
+            product.sort((a, b)=> a.name - b.name);
+            console.log(product);
+            setProducts(product)
+        }
+    }
 
+   /*  useEffect(()=>{
+    },[valueSelect]) */
 
     return(
         <div className="flex flex-col gap-y-[20px]">
             <PageRouting currentRuat={"Texanas"}/>
-            <Order />
+            {/* <Order /> */}
+            <div className="flex sm:flex-row flex-col gap-y-[15px] flex-wrap items-center w-[100%] sm:justify-between">
+                <section className="flex flex-row items-center gap-x-[10px]">
+                    <p>Ordenar por</p>
+                    <select name="select" className="border-2 border-colorBlack-400 w-[150px] h-[45px] outline-none flex items-center justify-center" onChange={handleOnchangeHightPrice}>
+                        <option value="mayor">Mayor Precio</option>
+                        <option value="menor">Menor Precio</option>
+                    </select>
+                </section>
+            </div>
+            <h1 className="text-center text-xl">Texanas</h1>
             <section className="flex flex-row md:justify-between justify-center flex-wrap w-[100%] gap-y-[50px] gap-x-[20px] py-16 ">
+
                 {
                     allProducts.map((item)=>{
                         const {_id,images,name,price,cantDues,newProduc} = item

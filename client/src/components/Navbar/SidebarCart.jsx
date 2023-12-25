5500028a0f705d0fc3a46db602a7c923c4cd39ed
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-12-25 17:54:43 -0300

    haciendl el total y boton comprar el sidebarCart

--- client/src/components/Navbar/SidebarCart.jsx
+++ client/src/components/Navbar/SidebarCart.jsx
@@ -1,5 +1,6 @@
 import { CgClose } from "react-icons/cg";
 import ItemsCart from "./ItemsCart";
+import { Button } from "@nextui-org/react";
 
 export default function SidebarCart({ isOpenCart, setIsOpenCart }) {
   return (
@@ -11,12 +12,19 @@ export default function SidebarCart({ isOpenCart, setIsOpenCart }) {
       <div className={`${!isOpenCart && "hidden"} pt-3`}>
         <div className="flex flex-col">
           <div className="flex items-center justify-between px-5 border-b-1 border-colorBlack-400 h-[50px]">
-            <h2 className=" text-lg font-bold">Carrito</h2>
+            <h2 className=" text-xl font-bold">Carrito</h2>
             <button className="" onClick={() => setIsOpenCart(false)}>
-              <CgClose size={25} color="black" />
+              <CgClose size={30} color="black" />
             </button>
           </div>
           <ItemsCart />
+          <section className="px-[20px] Py-[5px] border-t-1 border-colorBlack-400 flex flex-col gap-y-[10px]">
+            <article className="flex flex-row justify-between">
+                <h3 className=" text-lg font-light">Total</h3>
+                <p className=" text-lg font-semibold">$30.000</p>
+            </article>
+            <Button className=" bg-colorBlack-400 text-colorWhite-100 w-[100%] rounded-none">Completar Compra</Button>
+          </section>
         </div>
       </div>
     </div>

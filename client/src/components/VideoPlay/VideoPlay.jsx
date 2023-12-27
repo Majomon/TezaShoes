ec9ac7eb0e465fbe8e0d3784421b82dfcf8dea2f
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-26 22:30:57 -0300

    Video

--- client/src/components/VideoPlay/VideoPlay.jsx
+++ client/src/components/VideoPlay/VideoPlay.jsx
@@ -1,17 +1,51 @@
-'use client'
+import React from "react";
+import YouTube from "react-youtube";
+import style from "./VideoPlay.module.css";
 
-import ReactPlayer from "react-player";
+const VideoComponent = () => {
+  // Aquí puedes definir las opciones del video, como el tamaño, los parámetros de la URL, etc.
+  const opts = {
+    height: "315",
+    width: "560",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1, // Esto hace que el video se reproduzca automáticamente
+      controls: 0, // Esto hace que los controles de YouTube no se muestren
+      loop: 1, // Esto hace que el video se repita indefinidamente
+      playlist: "qtVOAtCAkPU", // Esto es necesario para que el loop funcione correctamente
+    },
+  };
 
-export default function VideoPlay(){
-    return(
-        <div className="w-[100%] flex items-center justify-center py-[50px]">
-            <ReactPlayer
-                url={"https://youtu.be/Et7TTfwvBFo?si=1R5xj_eS-u-QiVWH"}
-                height={500}
-                width={1920}
-                controls={false}
-                loop
-            />
-        </div>
-    )
-}
+  return (
+    <div>
+      <YouTube videoId="qtVOAtCAkPU" opts={opts} className={style.iframe} />
+    </div>
+  );
+};
+
+export default VideoComponent;
+
+/* import React, { useEffect, useRef } from 'react';
+
+const VideoPlayer = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.autoplay = true; // Reproducir automáticamente
+      videoRef.current.loop = true; // Repetir en bucle
+      videoRef.current.controls = false; // Ocultar controles
+    }
+  }, []);
+
+  return (
+    <div className="w-[100%] flex items-center justify-center py-[50px]">
+      <video ref={videoRef} width="100%" height="auto">
+        <source src="/Dota.mp4" type="video/mp4" />
+        Tu navegador no soporta el elemento de video.
+      </video>
+    </div>
+  );
+};
+
+export default VideoPlayer; */

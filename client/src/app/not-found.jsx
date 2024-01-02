2e47ecaff6846989fa89c2513fbe0610a4365acb
Ezequiel <ezgarcia@frba.utn.edu.ar>
2024-01-02 18:08:03 -0300

    haciendo el pageError con otro diseño

--- client/src/app/not-found.jsx
+++ client/src/app/not-found.jsx
@@ -1,20 +1,31 @@
+'use client'
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import fotoError from "../../assets/image/fotoError.png";
+import ImagePurse from "../../assets/svg/Purse.svg"
+import ImageShoes from "../../assets/svg/Shoes.svg"
+import { Button } from "@nextui-org/react";
 
 function NotFound() {
   return (
-    <main className="w-full min-h-screen text-black flex flex-col items-center justify-center overflow-hidden">
-      <Image src={fotoError} className="w-[150px] h-[150px]"/>
-      <p className="text-xl mt-2 mb-2 leading-tight tracking-wide ">
-        Página no encontrada, le recomendamos volver a
-      </p>
-      <p>
-        <Link href="/" className="">
-          <button className="text-3xl text-colorGold-800">INICIO</button>
-        </Link>
-      </p>
+    <main className="w-full min-h-screen text-black flex flex-row items-center justify-center overflow-hidden">
+      <div className="flex flex-col items-center gap-y-[30px]">
+        <h1 className=" text-4xl font-semibold">Ooops<span className="">!</span></h1>
+        {/* <Image src={fotoError} className="w-[150px] h-[150px]"/> */}
+        <p className=" text-2xl text-center w-96 leading-tight tracking-wide ">
+          No podemos encontrar la página 
+          que estás buscando :(
+        </p>
+        <Button className=" bg-colorBlack-400 text-colorWhite-100 rounded-none">REGRESAR</Button>
+      </div>
+      <div className=" border-1 w-[325px] h-[265px]">
+        <p>¿</p>
+        <p>¿</p>
+        <p>?</p>
+        <p>?</p>
+        <Image src={ImagePurse} width={180}/>
+        <Image src={ImageShoes} width={250}/>
+      </div>
     </main>
   );
 }
